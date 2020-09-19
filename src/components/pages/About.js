80f152f522c258cf98faabf682a425b13e080802
Alvaro Centeno <alvaroc0689@gmail.com>
2020-09-19 00:49:27 -0700

    created about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,10 +7,11 @@ const About = (props) => {
         <div>
             <Jumbo title={props.title} />
             <Content>
-                My name is Alvaro Centeno and I currently work as a Workplace Sevices Lead at Salesforce.
-                I graduated from San Francisco State University in 2011 with a Bachelors in Liberal Studies and English literature.
-                I am currently enrolled in a part-time course at UC Berkely to become a full stack developer and will be graduating on September 19th, 2020.
-                Once I complete my course, I want to become a software engineer.
+                <p> My name is Alvaro Centeno and I currently work as a Workplace Sevices Lead at Salesforce.</p>
+                <p>I graduated from San Francisco State University in 2011 with a Bachelors in Liberal Studies and English literature.</p>
+                <p>I am currently enrolled in a part-time course at UC Berkely to become a full stack developer and will be graduating on September 19th, 2020.</p>
+
+                <p>    Once I complete my course, I want to become a software engineer.</p>
             </Content>
         </div>
     )
