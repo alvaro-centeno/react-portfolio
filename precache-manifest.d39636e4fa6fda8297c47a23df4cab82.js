124409e2dbb438cabb53fdd4b666ffdd3909b0a1
Alvaro Centeno <alvaroc0689@gmail.com>
2020-09-19 01:59:46 -0700

    Updates

---
+++ precache-manifest.d39636e4fa6fda8297c47a23df4cab82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317c66c2bce9cc202e016e06abb281ef",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0fa2eb4a819946d567a9",
+    "url": "/react-portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f14be4fdf16b83a27f9d",
+    "url": "/react-portfolio/static/css/main.c00e05aa.chunk.css"
+  },
+  {
+    "revision": "0fa2eb4a819946d567a9",
+    "url": "/react-portfolio/static/js/2.ff8157ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.ff8157ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14be4fdf16b83a27f9d",
+    "url": "/react-portfolio/static/js/main.92d0df53.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "dcef3abedf0e0761203aaeb85886a6f3",
+    "url": "/react-portfolio/static/media/github.dcef3abe.jpg"
+  },
+  {
+    "revision": "d8aba978cfff0ad8539ef614db01dfaf",
+    "url": "/react-portfolio/static/media/linkedin.d8aba978.jpg"
+  }
+]);
